97dd68a20e23147c9fef6bc11fcd46ba792b7e28
LucasDibz <lucas.f.dib@gmail.com>
2025-01-17 13:29:29 +0000

    change text

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -2,7 +2,7 @@ export const navigation = [
   { label: 'About us', href: 'about-us' },
   { label: 'Scientific Publications', href: 'scientific-publications' },
   { label: 'Conferences', href: 'conferences' },
-  { label: 'Course', href: 'courses' },
+  { label: 'Summer Course', href: 'courses' },
   { label: 'SINPL-EU', href: 'sinpl-eu' },
   { label: 'Contacts', href: 'contacts' },
 ];
