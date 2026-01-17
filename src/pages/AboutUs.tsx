763168637e37f7d003d95b8cd9975bb98e44a034
Lucas Dib <lucas.f.dib@gmail.com>
2026-01-17 16:27:18 +0000

    fix: home container

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,12 +1,12 @@
-import { Activities, Body } from '../components';
+import { Activities } from '../components';
 import { AboutUs as AboutUsInfo, Ipsers } from '../components/AboutUs';
 
 export function AboutUs() {
   return (
-    <Body className='pt-0'>
+    <>
       <AboutUsInfo />
       <Activities />
       <Ipsers />
-    </Body>
+    </>
   );
 }
