f4939c6494326948dff7e36701d1ced498d3772f
Lucas Dib <lucas.f.dib@gmail.com>
2026-02-23 14:51:30 +0000

    refactor: sinpl projects as separate cards

--- src/components/SINPL/SinplProjects.tsx
+++ src/components/SINPL/SinplProjects.tsx
@@ -1,53 +1,28 @@
-import { useState } from 'react';
-import { Card } from '../';
 import { SinplProjectCard } from './SinplProjectCard';
 
 const projects = [
   {
-    label: 'Year 1',
+    title: 'Project Year 1',
     imageSrc: './images/sinpl-eu-project-1.jpeg',
     pdfFile: './Course syllabus year1.pdf',
   },
   {
-    label: 'Year 2',
-    title: 'Midterm seminar "Pathways of IP law teaching"',
+    title: 'Project Year 2',
+    label: 'Midterm seminar "Pathways of IP law teaching"',
     pdfFile: './Course syllabus year2.pdf',
   },
   {
-    label: 'Year 3',
-    title: 'Final conference "Sustainability through EU IP Law"',
+    title: 'Project Year 3',
+    label: 'Final conference "Sustainability through EU IP Law"',
   },
 ];
 
 export function SinplProjects() {
-  const [activeProject, setActiveProject] = useState(0);
-  const project = projects[activeProject];
-
   return (
-    <Card className='bg-white rounded-[3rem] max-w-full'>
-      <Card.Title className='text-heading'>Projects</Card.Title>
-      <ul className='-mb-3 flex flex-wrap text-sm'>
-        {projects.map((p, i) => (
-          <li key={p.label}>
-            <button
-              type='button'
-              className='inline-block p-4 text-lg font-bold text-slate-400 rounded-t-lg hover:cursor-pointer hover:shadow-inner transition data-[active=true]:shadow-inner data-[active=true]:text-heading'
-              data-active={i === activeProject}
-              onClick={() => setActiveProject(i)}
-            >
-              {p.label}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <Card.Content>
-        <SinplProjectCard
-          key={activeProject}
-          title={project.title}
-          imageSrc={project.imageSrc}
-          pdfFile={project.pdfFile}
-        />
-      </Card.Content>
-    </Card>
+    <div className='flex flex-col gap-6'>
+      {projects.map((project) => (
+        <SinplProjectCard key={project.title} project={project} />
+      ))}
+    </div>
   );
 }
